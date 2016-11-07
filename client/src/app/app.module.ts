91c6f32a9f405b1411a4490e3ea332948de3904a
Aaron Layfield <Aaron.Layfield@gmail.com>
2016-11-07 12:53:14 +0900

    Introduced baseline template converting to ng components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LeftNavComponent
   ],
   imports: [
     BrowserModule,
